bd731faa942f9b5a379884b028a6bfdc6ad76e79
Kenneth Chow <kennethjchow@gmail.com>
2020-08-18 20:41:17 -0700

    Adding paths

--- src/components/mainNavbar/MainNavbar.js
+++ src/components/mainNavbar/MainNavbar.js
@@ -7,21 +7,14 @@ class MainNavbar extends React.Component {
       return (
         <div>
           <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">TutorTools</Navbar.Brand>
+            <Navbar.Brand href="/">TutorTools</Navbar.Brand>
             <Nav className="mr-auto">
               <NavDropdown title="Learning" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Item href="practiceVowels">VowelPractice</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
               <Nav.Link href="#practice">Practice</Nav.Link>
            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-info">Search</Button>
-            </Form>
           </Navbar>
         </div>
       );
