754087789650e6eb13a70e67ef91c0ccef648bff
Kenneth Chow <kennethjchow@gmail.com>
2020-08-26 08:07:42 -0700

    Align center logo

--- src/components/mainNavbar/MainNavbar.js
+++ src/components/mainNavbar/MainNavbar.js
@@ -1,5 +1,5 @@
 import React from '../../../node_modules/react';
-import {Navbar, Nav, NavDropdown} from '../../../node_modules/react-bootstrap'
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from '../../../node_modules/react-bootstrap'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './MainNavbar.css'
 
@@ -7,16 +7,25 @@ class MainNavbar extends React.Component {
     render() {
       return (
         <div className="navContainer">
-          <Navbar bg="light" variant="light">
-            <Navbar.Brand  href="/">
-              <div className="navTitle">Tutor Tools</div></Navbar.Brand>
-            <Nav className="mr-auto" className="navItem">
-              <NavDropdown title="Learning" id="basic-nav-dropdown">
-                <NavDropdown.Item href="practiceVowels">VowelPractice</NavDropdown.Item>
+          <Navbar bg="light" variant="light" className="main-navbar">
+            <Nav className="mr-auto" className="leftNavBarItems" className="order-1" className="w-100">
+              <NavDropdown className="navItem" title="Learning" id="basic-nav-dropdown">
+                <NavDropdown.Item href="practiceVowels" className="navItem">VowelPractice</NavDropdown.Item>
                 <NavDropdown.Divider />
               </NavDropdown>
-              <Nav.Link href="practice">Practice</Nav.Link>
-           </Nav>
+              <Nav.Link href="practice" className="navItem">Practice</Nav.Link>
+            </Nav>
+            <Nav className="mx-auto" className="order-0">
+              <Navbar.Brand  href="/" className="brandItem">
+                <div className="navTitle">Tutor Tools</div>
+              </Navbar.Brand>
+            </Nav>    
+            <Nav className="rightNavBarItems" className="order-2" className="w-100">
+              <Form inline className="ml-auto">
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-primary">Search</Button>
+              </Form>
+            </Nav>
           </Navbar>
         </div>
       );
